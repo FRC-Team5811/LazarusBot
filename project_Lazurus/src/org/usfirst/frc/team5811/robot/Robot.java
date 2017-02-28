ab3759c21c37b35bc6c710b0ca7ea6f1fe1ea011
Sam Sidhu <titanstrykr442@gmail.com>
2017-02-28 18:28:17 -0500

    Instantiated the NavX

--- project_Lazurus/src/org/usfirst/frc/team5811/robot/Robot.java
+++ project_Lazurus/src/org/usfirst/frc/team5811/robot/Robot.java
@@ -2,9 +2,13 @@
 package org.usfirst.frc.team5811.robot;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
@@ -39,6 +43,8 @@ public class Robot extends IterativeRobot {
     Victor backLeftDriveMotor;
     Victor backRightDriveMotor;
 	
+    Encoder drive;
+    
 	Joystick joystick;
 
     JoystickButton logitechY;
@@ -47,6 +53,10 @@ public class Robot extends IterativeRobot {
     JoystickButton logitechB;
     JoystickButton logitechLeftBumper;
     JoystickButton logitechRightBumper;
+    JoystickButton logitechBack;
+    
+	boolean shouldBeRunningCorrect;
+	boolean wasPressedBackButton;
     
     PowerDistributionPanel power = new PowerDistributionPanel();
     
@@ -55,6 +65,13 @@ public class Robot extends IterativeRobot {
 
     double throttleGain;
     double turningGain;
+    
+    double rotationPos;
+    
+    int rotationCountForDrive;
+	double rotationRateForDrive;
+	double distance;
+	
     
     int cycleCounter;
     
@@ -75,6 +92,37 @@ public class Robot extends IterativeRobot {
     	
     	driveMotors(leftSpeed, rightSpeed);
     	
+    }
+    
+	private void testForCorrectionMode() {
+		// CORRECTION MODE
+		if (logitechBack.get()) {
+			if (!wasPressedBackButton) {
+				rotationPos = (float) ahrs.getAngle();
+				shouldBeRunningCorrect = !shouldBeRunningCorrect;
+			}
+			wasPressedBackButton = true;
+		} else {
+			wasPressedBackButton = false;
+		}
+
+		if (shouldBeRunningCorrect) {
+			correct();
+			System.out.println("in correct mode");
+		} else {
+			System.out.println("not in correct mode");
+		}
+	}
+	private void correct() {
+		float nowRot = (float) ahrs.getAngle();
+		System.out.println(rotationPos);
+		System.out.println(nowRot);
+		if (nowRot >= rotationPos + 10) {
+			driveMotors(-.5,-.5);
+		}
+		if (nowRot <= rotationPos - 10) {
+			driveMotors(.5,.5);
+		}
     }
     
     private void turnMacro(float degrees){
@@ -119,19 +167,42 @@ public class Robot extends IterativeRobot {
 		// y 4
 		// b 3
 		// a 2
-		// x 1
+		// x 1 
+		
+		shouldBeRunningCorrect = false;
+	    wasPressedBackButton = false;
 	
+	    
 		
 		joystick = new Joystick(0);
 		logitechY = new JoystickButton(joystick, 4);
 		logitechX = new JoystickButton(joystick, 1);
 		logitechB = new JoystickButton(joystick, 3);
 		logitechA = new JoystickButton(joystick, 2);
+		logitechBack = new JoystickButton(joystick, 9);
+		
 		
 	     frontLeftDriveMotor = new Victor(2);
 	       frontRightDriveMotor = new Victor(0);
 	       backLeftDriveMotor = new Victor(3); 
 	       backRightDriveMotor = new Victor(1);
+	       
+	       
+	       
+			drive = new Encoder(8,9, true, Encoder.EncodingType.k4X);
+			drive.setMaxPeriod(1);
+			drive.setDistancePerPulse(36);
+			drive.setMinRate(10);
+			drive.setSamplesToAverage(32);
+			
+			try {
+				ahrs = new AHRS(SerialPort.Port.kUSB);
+				//ahrs = new AHRS(I2C.Port.kMXP); //WE WILL NEED I2C IN THE FUTURE.
+				// RIGHT NOW WE WILL STICK WITH USB
+			} catch (RuntimeException ex) {
+				System.out.println("NavX instantiation error");
+				DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+			}
 	}
 
 	
@@ -145,6 +216,139 @@ public class Robot extends IterativeRobot {
 		Scheduler.getInstance().run();
 	}
 
+	private void operatorControl() {
+		if (isOperatorControl() && isEnabled()) {
+
+			Timer.delay(0.020); /* wait for one motor update time period (50Hz) */
+
+			/*
+			 * boolean zero_yaw_pressed = stick.getTrigger(); if (
+			 * zero_yaw_pressed ) { ahrs.zeroYaw(); }
+			 */
+
+			/* Display 6-axis Processed Angle Data */
+			/*
+			SmartDashboard.putBoolean("IMU_Connected", ahrs.isConnected());
+			SmartDashboard.putBoolean("IMU_IsCalibrating", ahrs.isCalibrating());
+			SmartDashboard.putNumber("IMU_Yaw", ahrs.getYaw());
+			SmartDashboard.putNumber("IMU_Pitch", ahrs.getPitch());
+			SmartDashboard.putNumber("IMU_Roll", ahrs.getRoll());
+
+			/* Display tilt-corrected, Magnetometer-based heading (requires */
+			/* magnetometer calibration to be useful) */
+
+			//SmartDashboard.putNumber("IMU_CompassHeading", ahrs.getCompassHeading());
+
+			/*
+			 * Display 9-axis Heading (requires magnetometer calibration to be
+			 * useful)
+			 */
+		//	SmartDashboard.putNumber("IMU_FusedHeading", ahrs.getFusedHeading());
+
+			/*
+			 * These functions are compatible w/the WPI Gyro Class, providing a
+			 * simple
+			 */
+			/* path for upgrading from the Kit-of-Parts gyro to the navx MXP */
+
+			SmartDashboard.putNumber("IMU_TotalYaw", ahrs.getAngle());
+			//SmartDashboard.putNumber("IMU_YawRateDPS", ahrs.getRate());
+
+			/*
+			 * Display Processed Acceleration Data (Linear Acceleration, Motion
+			 * Detect)
+			 */
+/*
+			SmartDashboard.putNumber("IMU_Accel_X", ahrs.getWorldLinearAccelX());
+			SmartDashboard.putNumber("IMU_Accel_Y", ahrs.getWorldLinearAccelY());
+			SmartDashboard.putBoolean("IMU_IsMoving", ahrs.isMoving());
+			SmartDashboard.putBoolean("IMU_IsRotating", ahrs.isRotating());
+*/
+			/*
+			 * Display estimates of velocity/displacement. Note that these
+			 * values are
+			 */
+			/*
+			 * not expected to be accurate enough for estimating robot position
+			 * on a
+			 */
+			/*
+			 * FIRST FRC Robotics Field, due to accelerometer noise and the
+			 * compounding
+			 */
+			/*
+			 * of these errors due to single (velocity) integration and
+			 * especially
+			 */
+			/* double (displacement) integration. */
+/*
+			SmartDashboard.putNumber("Velocity_X", ahrs.getVelocityX());
+			SmartDashboard.putNumber("Velocity_Y", ahrs.getVelocityY());
+			SmartDashboard.putNumber("Displacement_X", ahrs.getDisplacementX());
+			SmartDashboard.putNumber("Displacement_Y", ahrs.getDisplacementY());
+*/
+			/* Display Raw Gyro/Accelerometer/Magnetometer Values */
+			/*
+			 * NOTE: These values are not normally necessary, but are made
+			 * available
+			 */
+			/*
+			 * for advanced users. Before using this data, please consider
+			 * whether
+			 */
+			/* the processed data (see above) will suit your needs. */
+/*
+			SmartDashboard.putNumber("RawGyro_X", ahrs.getRawGyroX());
+			SmartDashboard.putNumber("RawGyro_Y", ahrs.getRawGyroY());
+			SmartDashboard.putNumber("RawGyro_Z", ahrs.getRawGyroZ());
+			SmartDashboard.putNumber("RawAccel_X", ahrs.getRawAccelX());
+			SmartDashboard.putNumber("RawAccel_Y", ahrs.getRawAccelY());
+			SmartDashboard.putNumber("RawAccel_Z", ahrs.getRawAccelZ());
+			SmartDashboard.putNumber("RawMag_X", ahrs.getRawMagX());
+			SmartDashboard.putNumber("RawMag_Y", ahrs.getRawMagY());
+			SmartDashboard.putNumber("RawMag_Z", ahrs.getRawMagZ());
+			SmartDashboard.putNumber("IMU_Temp_C", ahrs.getTempC());
+			SmartDashboard.putNumber("IMU_Timestamp", ahrs.getLastSensorTimestamp());
+*/
+			/* Omnimount Yaw Axis Information */
+			/*
+			 * For more info, see
+			 * http://navx-mxp.kauailabs.com/installation/omnimount
+			 */
+		//	AHRS.BoardYawAxis yaw_axis = ahrs.getBoardYawAxis();
+			/*
+			SmartDashboard.putString("YawAxisDirection", yaw_axis.up ? "Up" : "Down");
+			SmartDashboard.putNumber("YawAxis", yaw_axis.board_axis.getValue());
+
+			/* Sensor Board Information */
+			//SmartDashboard.putString("FirmwareVersion", ahrs.getFirmwareVersion());
+			/* Quaternion Data */
+			/*
+			 * Quaternions are fascinating, and are the most compact
+			 * representation of
+			 */
+			/*
+			 * orientation data. All of the Yaw, Pitch and Roll Values can be
+			 * derived
+			 */
+			/*
+			 * from the Quaternions. If interested in motion processing,
+			 * knowledge of
+			 */
+			
+			/* Quaternions is highly recommended. */
+			/*
+			SmartDashboard.putNumber("QuaternionW", ahrs.getQuaternionW());
+			SmartDashboard.putNumber("QuaternionX", ahrs.getQuaternionX());
+			SmartDashboard.putNumber("QuaternionY", ahrs.getQuaternionY());
+			SmartDashboard.putNumber("QuaternionZ", ahrs.getQuaternionZ());
+
+			/* Connectivity Debugging Support */
+			SmartDashboard.putNumber("IMU_Byte_Count", ahrs.getByteCount());
+			SmartDashboard.putNumber("IMU_Update_Count", ahrs.getUpdateCount());
+		}
+	}
+	
 	@Override
 	public void autonomousInit() {
 		autonomousCommand = chooser.getSelected();
@@ -162,6 +366,7 @@ public class Robot extends IterativeRobot {
 		
 		cycleCounter++;
 		
+		distance = drive.getDistance();
 		
 		if(autoSelecter == 0){
 			if(cycleCounter > 0 && cycleCounter <= 1)
@@ -182,7 +387,22 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 		
-		arcadeDrive((joystick.getRawAxis(2)) * 0.6, -(joystick.getRawAxis(1)) * 0.6); 
+		distance = drive.getDistance();
+		
+		System.out.println("Distance: " + distance);
+		
+		rotationCountForDrive = drive.get();
+		rotationRateForDrive = drive.getRate();
+		
+		
+	     frontLeftDriveMotor.set(joystick.getX()-joystick.getY());
+	     frontRightDriveMotor .set(joystick.getX() + joystick.getY());
+	     backLeftDriveMotor .set(joystick.getX()-joystick.getY());
+	     backRightDriveMotor .set(joystick.getX()+joystick.getY());
+
+	     //operatorControl();
+	     
+		//arcadeDrive(-joystick.getRawAxis(1), joystick.getRawAxis(2)); 
 	}
 
 	/**
